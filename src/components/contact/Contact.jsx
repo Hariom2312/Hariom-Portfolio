07267720002fa90d0a079f26e4377177dbcb57f4
Hariom Bamboriya <dhakadhariom007@gmail.com>
2024-06-28 14:37:45 +0530

    finally done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -102,6 +102,7 @@ const Contact = () => {
             method="POST"
             className="flex justify-center items-center flex-col gap-5 w-[70%] md:w-[100%] sm:w-[95%] mx-auto"
             action="mailto:dhakadhariom007@gmail.com"
+            autocomplete="off"
           >
             <input
               className="px-3 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
